b24bae94ca878e0546a9836f07adc5c48d4c501d
Daorse <mmorpgplays5@gmail.com>
2025-01-13 21:26:18 +0100

    Ending design of components search on Mobile need responsive after

--- src/components/searchBar.svelte
+++ src/components/searchBar.svelte
@@ -1,14 +1,22 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import { Map, MapPin, MapPinHouse, MapPinPlusIcon, Search } from 'lucide-svelte';
+</script>
 
-<div class="border-2 border-black">
-	<input
-		type="text"
-		class=" h-[50px]border-2 Agdasima w-[300px] border-black text-center"
-		placeholder="Recherche..."
-	/>
-	<input
-		type="text"
-		class=" h-[50px]border-2 Agdasima w-[300px] border-black text-center"
-		placeholder="Recherche..."
-	/>
+<div class="flex flex-col items-center gap-4 p-4 md:flex-row md:justify-center md:gap-6">
+	<div class="relative">
+		<input
+			type="text"
+			class="Agdasima inputshadow relative h-[50px] w-[300px] rounded-[10px] border-black bg-[#78788010] text-center text-[#212121] placeholder:text-[#21212150]"
+			placeholder="Recherche..."
+		/>
+		<Search class="absolute right-2 top-[14px]" />
+	</div>
+	<div class="relative">
+		<input
+			type="text"
+			class="Agdasima inputshadow h-[50px] w-[300px] rounded-[10px] border-black bg-[#78788010] text-center text-[#212121] placeholder:text-[#21212150]"
+			placeholder="Localisation..."
+		/>
+    <MapPin class="absolute right-2 top-[14px]"/>
+	</div>
 </div>
