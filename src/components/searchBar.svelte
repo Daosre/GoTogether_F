cf1cf90aef1863ade2623629838cccda0edc6bc1
Daorse <mmorpgplays5@gmail.com>
2025-01-14 11:21:00 +0100

    Ending responsive and design of components searchbar

--- src/components/searchBar.svelte
+++ src/components/searchBar.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { Map, MapPin, MapPinHouse, MapPinPlusIcon, Search } from 'lucide-svelte';
+	import { MapPin, Search } from 'lucide-svelte';
 </script>
 
-<div class="flex flex-col items-center gap-4 p-4 md:flex-row md:justify-center md:gap-6">
+<div class="flex flex-col items-center gap-4 p-4 md:flex-row md:justify-center md:gap-6 lg:gap-20 ">
 	<div class="relative">
 		<input
 			type="text"
-			class="Agdasima inputshadow relative h-[50px] w-[300px] rounded-[10px] border-black bg-[#78788010] text-center text-[#212121] placeholder:text-[#21212150]"
+			class="Agdasima inputshadow h-[50px] w-[300px] rounded-[10px] border-black bg-[#78788010] text-center text-[#212121] placeholder:text-[#21212150] lg:w-[400px]"
 			placeholder="Recherche..."
 		/>
 		<Search class="absolute right-2 top-[14px]" />
@@ -14,9 +14,9 @@
 	<div class="relative">
 		<input
 			type="text"
-			class="Agdasima inputshadow h-[50px] w-[300px] rounded-[10px] border-black bg-[#78788010] text-center text-[#212121] placeholder:text-[#21212150]"
+			class="Agdasima inputshadow h-[50px] w-[300px] rounded-[10px] border-black bg-[#78788010] text-center text-[#212121] placeholder:text-[#21212150] lg:w-[400px]"
 			placeholder="Localisation..."
 		/>
-    <MapPin class="absolute right-2 top-[14px]"/>
+		<MapPin class="absolute right-2 top-[14px]" />
 	</div>
 </div>
