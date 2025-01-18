69e375a3a0bb0ac72473d67dafa8c65b01c065ca
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-01-18 18:21:14 +0100

    validator event schema

--- src/validator/event.ts
+++ src/validator/event.ts
@@ -5,5 +5,27 @@ export const schemaEvent = yup.object({
 		.required('Ce champ est requis !')
 		.min(10, 'Minimum 10 caractères')
 		.max(50, 'Maximum 50 caractères'),
-    categoryName: yup.string().required
+	categoryName: yup
+		.string()
+		.required('Ce champ est requis !')
+		.min(4, 'Minimum 4 caractères')
+		.max(50, 'Maximum 50 caractères'),
+	city: yup.string().required('Ce champ est requis !').max(30, 'Maximum 30 caractères'),
+	address: yup
+		.string()
+		.required('Ce champ est requis !')
+		.min(3, 'Minimum 3 caractères')
+		.max(255, 'Maximum 255 caractères'),
+	time: yup.string().required('Ce champ est requis !'),
+	maxParticipants: yup
+		.number()
+		.required('Ce champ est requis !')
+		.min(2, 'Minimum 2 personnes')
+		.max(999, 'Maximum 999'),
+	price: yup
+		.number()
+		.required('Ce champ est requis !')
+		.min(0, 'Minimum 0')
+		.max(9999, 'Maximum 9999'),
+	description: yup.string().required('Ce champ est requis !').max(500, 'Maximum 500 caractères'),
 });
